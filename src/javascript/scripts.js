f5d002015f68f14ffe82cd8c5d17f6258aa04ad1
Júlia Martins <143566807+juliaoliveiramarttins@users.noreply.github.com>
2025-01-02 12:20:37 -0300

    add fechamento do header após seleção no JS

--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -1,7 +1,19 @@
-$(document).ready(function(){
-    $('#mobile_btn').on('click', function (){
-        $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x');
+$(document).ready(function() {
+    // Abrir e fechar o menu quando o botão de menu for clicado
+    $('#mobile_btn').on('click', function() {
+        $('#mobile_menu').toggleClass('active');  // Alterna a classe 'active' no menu
+        $('#mobile_btn').find('i').toggleClass('fa-x');  // Alterna o ícone entre "fa-bars" e "fa-x"
+    });
+
+    // Fechar o menu quando um item de navegação for clicado
+    $('#mobile_nav_list .nav-item').on('click', function() {
+        $('#mobile_menu').removeClass('active');  // Remove a classe 'active' para esconder o menu
+        $('#mobile_btn').find('i').removeClass('fa-x');  // Restaura o ícone do menu para "fa-bars"
+    });
+
+    // Fechar o menu ao clicar no botão "comprar"
+    $('.btn-default').on('click', function() {
+        $('#mobile_menu').removeClass('active');  // Fecha o menu quando o botão "comprar" é clicado
+        $('#mobile_btn').find('i').removeClass('fa-x');  // Restaura o ícone do menu
     });
 });
-
